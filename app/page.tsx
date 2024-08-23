b1e958ff2cce47a0ad764d1e2fbceaebdedcbdcb
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-08-22 22:08:55 -0300

    Orange by Gray

--- app/page.tsx
+++ app/page.tsx
@@ -13,19 +13,19 @@ export default function Home() {
             </div>
             <div className="my-4 w-full flex justify-center gap-x-2 md:gap-x-4">
                 <div className="text-center w-14 sm:w-28">
-                    <div className="count bg-white my-4 p-2 md:p-4 text-xl md:text-4xl font-medium text-blue-900 border-2 border-orange-700 rounded-md">10</div>
+                    <div className="count bg-white my-4 p-2 md:p-4 text-xl md:text-4xl font-medium text-blue-900 border-2 border-gray-400 rounded-md">10</div>
                     <div className="bg-blue-900 text-[8px] sm:text-xs md:text-base my-2 px-2 md:px-4 py-2 rounded-full text-white">DIAS</div>
                 </div>
                 <div className="text-center w-14 sm:w-28">
-                    <div className="count bg-white my-4 p-2 md:p-4 text-xl md:text-4xl font-medium text-blue-900 border-2 border-orange-700 rounded-md">3</div>
+                    <div className="count bg-white my-4 p-2 md:p-4 text-xl md:text-4xl font-medium text-blue-900 border-2 border-gray-400 rounded-md">3</div>
                     <div className="bg-blue-900 text-[8px] sm:text-xs md:text-base my-2 px-2 md:px-4 py-2 rounded-full text-white">HORAS</div>
                 </div>
                 <div className="text-center w-14 sm:w-28">
-                    <div className="count bg-white my-4 p-2 md:p-4 text-xl md:text-4xl font-medium text-blue-900 border-2 border-orange-700 rounded-md">15</div>
+                    <div className="count bg-white my-4 p-2 md:p-4 text-xl md:text-4xl font-medium text-blue-900 border-2 border-gray-400 rounded-md">15</div>
                     <div className="bg-blue-900 text-[8px] sm:text-xs md:text-base my-2 px-2 md:px-4 py-2 rounded-full text-white">MINUTOS</div>
                 </div>
                 <div className="text-center w-14 sm:w-28">
-                    <div className="count bg-white my-4 p-2 md:p-4 text-xl md:text-4xl font-medium text-blue-900 border-2 border-orange-700 rounded-md">12</div>
+                    <div className="count bg-white my-4 p-2 md:p-4 text-xl md:text-4xl font-medium text-blue-900 border-2 border-gray-400 rounded-md">12</div>
                     <div className="bg-blue-900 text-[8px] sm:text-xs md:text-base my-2 px-2 md:px-4 py-2 rounded-full text-white">SEGUNDOS</div>
                 </div>
             </div>
@@ -33,19 +33,19 @@ export default function Home() {
             <div className="my-4 flex md:flex-row flex-col gap-x-4 rounded-xl p-2">
                 <a href="mailto:contato@brunabrumoficial.com.br" target="_blank">
                     <div className="my-2 md:my-4 flex text-base font-medium text-blue-900 justify-center items-center">
-                        <i className="fa-regular fa-envelope text-orange-700 text-xl mx-2"></i>
+                        <i className="fa-regular fa-envelope text-gray-border-gray-400 text-xl mx-2"></i>
                         <div className="mx-2">contato@brunabrumoficial.com.br</div>
                     </div>
                 </a>
                 <a href="tel:+5521964646334" target="_blank">
                     <div className="my-4 flex base-xl font-medium text-blue-900 justify-center items-center">
-                        <i className="fa-solid fa-phone text-orange-700 text-xl mx-2"></i>
+                        <i className="fa-solid fa-phone text-gray-border-gray-400 text-xl mx-2"></i>
                         <div className="mx-2">(21) 99999-9999</div>
                     </div>
                 </a>
                 <a href="https://api.whatsapp.com/send?phone=5521992260747" target="_blank">
                     <div className="my-2 md:my-4 flex text-base font-medium text-blue-900 justify-center items-center">
-                        <i className="fa-brands fa-whatsapp text-orange-700 text-xl mx-2"></i>
+                        <i className="fa-brands fa-whatsapp text-gray-border-gray-400 text-xl mx-2"></i>
                         <div className="mx-2">(21) 99999-9999</div>
                     </div>
                 </a>
