6c83efa0a503da55121947a4d74e64fe2f5e90f9
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-12-30 14:08:18 -0300

    Atualizado Final

--- app/parts/home/FaixaTitulo.tsx
+++ app/parts/home/FaixaTitulo.tsx
@@ -10,7 +10,7 @@ function NomeFaixa() {
 
 export default function FaixaTitulo() {
     return (
-        <div className="bg-blue-500 py-2 md:py-4 overflow-x-hidden relative text-white font-bodoni">
+        <div className="bg-blue-500 py-2 md:py-4  overflow-x-hidden relative text-white font-bodoni">
             <div className="text-xl md:text-2xl flex justify-center gap-x-10 uppercase">
                 <NomeFaixa />
                 <NomeFaixa />
