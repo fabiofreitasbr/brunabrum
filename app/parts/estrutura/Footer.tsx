027c204a32bee7ad3a9e689d811dccbd6fd8a52c
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-12-30 15:03:45 -0300

    Foto - Ajuste

--- app/parts/estrutura/Footer.tsx
+++ app/parts/estrutura/Footer.tsx
@@ -4,16 +4,16 @@ export default function Rodape() {
     return (
         <footer className="bg-blue-500 py-8 md:py-16">
             <div className="container mx-auto text-white px-4 ">
-                <div className="md:grid md:grid-cols-2">
+                <div className="flex flex-col md:flex-row gap-10 justify-between items-center">
                     <div>
-                        <p className=" pt-2 text-center md:text-left text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bodoni my-1 md:my-3">
+                        <p className="text-center md:text-left text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bodoni mb-1 md:mb-3">
                             BRUNA BRUM
                         </p>
                         <p className="text-center md:text-left text-xs md:text-base lg:text-lg xl:text-xl">
                             Todos os direitos reservados.
                         </p>
                     </div>
-                    <div className="pt-14 md:flex md:flex-col md:items-end md:justify-center">
+                    <div className="md:flex md:flex-col md:items-end md:justify-center">
                         <div>
                             <RodapeRedesSociais />
                         </div>
